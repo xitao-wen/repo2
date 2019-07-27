dd46ad95d0afa5ab3f246d6b431f30f2678c64f0
keke <keke@itcast.cn>
2019-07-27 18:10:57 +0800

    使用HTTPSpush

--- src/com/itheima/controller/Test.java
+++ src/com/itheima/controller/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("git测试...");
         System.out.println("dev1分支测试...");
+        System.out.println("使用HTTPSpush");
     }
 }
